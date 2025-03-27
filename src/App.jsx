523969bdd0464859c4f4aa8083153d5854c26771
Mani <manicse9080@gmail.com>
2025-03-27 14:11:00 +0530

    New project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import About from './Components/About/About'
+import Experience from './Components/Experience/Experience'
+import Home from './Components/Home/Home'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+  return (
+    <>
+     <Navbar />
+     <Home />
+     <About />
+     <Experience /> 
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/About/About.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import myImage from '/img/me.jpg';
+import { GiGraduateCap} from "react-icons/gi";
+import { BsTelephoneForward } from 'react-icons/bs';
+import { TfiWorld } from 'react-icons/tfi';
+import './About.css';
+
+
+function About() {
+    return ( 
+    <>
+       <section>
+         <h5>Get To Know</h5>
+         <h2>About Me</h2>
+
+         <div className="container about_container">
+            <div className="about_me">
+                <div className="about_me_image">
+                    <img src={myImage} alt="Mani" />
+                </div>
+            </div>
+
+            <div className="about_content">
+                <div className="about_cards">
+                    <article className="about_card">
+                        <GiGraduateCap className='about_icon'/>
+                        <h5>Degree</h5>
+                        <small>
+                            BE.Computer Science <br />
+                            <i>SSMIET Dindigul</i>
+                        </small>
+                    </article>
+
+                    <article className="about_card">
+                        <TfiWorld  className='about_icon'/>
+                        <h5>Location</h5>
+                        <small>
+                           Address...
+                        </small>
+                    </article>
+
+                    <article className="about_card">
+                        <BsTelephoneForward  className='about_icon'/>
+                        <h5>Contact</h5>
+                        <small>
+                          <ul>
+                            <li>Phone: </li>
+                            <li>EMail: </li>
+                          </ul>
+                        </small>
+                    </article>
+
+                </div>
+                <p>
+                    Hi, I'm Manikandan B, a Skilled full-stack developer.
+                    Hi, I'm Manikandan B, a Skilled full-stack developer.
+                    Hi, I'm Manikandan B, a Skilled full-stack developer.
+                </p>
+            </div>
+
+         </div>
+       </section>
+    </>
+     );
+}
+
+export default About;
